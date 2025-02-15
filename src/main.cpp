7072837b52b5b637e2a225965840feedf7a6d52d
anzisheng <anzisheng@163.com>
2025-02-15 10:54:11 +0800

    translate the bank demo

--- src/main.cpp
+++ src/main.cpp
@@ -105,6 +105,198 @@ void write_json_file(string file_name, json& commands)
     std::ofstream file(file_name);
     file << commands;
 }
+//////////////////////////////////////
+#include <thread>
+#include <iostream>
+#include <queue>
+#include <functional>
+#include <fstream>
+#include <mutex>
+#include <condition_variable>
+using namespace std;
+
+template <typename T>
+class MutexSafe
+{
+private:
+    mutex _mutex;
+    T* _resource;
+    T* operator ->(){}
+    T& operator &(){}
+public:
+    MutexSafe(T* resource):_resource(resource){}
+    ~MutexSafe(){delete _resource;}
+    void lock()
+    {
+        _mutex.lock();
+    }
+    void unlock()
+    {
+        _mutex.unlock();
+    }
+    bool try_lock()
+    {
+        return _mutex.try_lock();
+    }
+    mutex& Mutex()
+    {
+        return _mutex;
+    }
+    T& Acquire (unique_lock<MutexSafe<T>>& lock)
+    {
+        MutexSafe<T> *_safe = lock.mutex();
+        if(&_safe->Mutex()!=&_mutex)
+        {
+            throw "wrong lock object passed to Acquire function.\n";
+        }
+        return *_resource;
+    }
+    T& Acquire (unique_lock<mutex>& lock)
+    {
+        if(lock.mutex()!=&_mutex)
+        {
+            throw "wrong lock object passed to Acquire function.\n";
+        }
+        return *_resource;
+    }
+};
+
+template <typename MsgType>
+class MsgQueue
+{
+private:
+    queue<MsgType> _queue; 
+    mutex _mutex;
+    condition_variable _enqCv;
+    condition_variable _deqCv;
+    int _limit;
+public:
+    MsgQueue(int limit=3):_limit(limit){
+        std::cout << "队列长度最大为："<< _limit << "。" << std::endl;
+    }
+    void Enqueue(MsgType & msg)
+    {
+        unique_lock<mutex> lock(_mutex);
+        if(_queue.size()>=_limit)
+        {
+             cout<<"queue is full, wait()..."<<endl;
+            _enqCv.wait(lock,[this]{return _queue.size()<_limit;});
+        }
+        _queue.push(msg);
+        cout << "now ,the queue length is : " <<_queue.size() <<endl;
+        cout << "I have added one task, any one thread can dequeue the task"<<endl;
+        _deqCv.notify_one();
+    }
+    MsgType& Dequeue()
+    {
+        unique_lock<mutex> lock(_mutex);
+        if(_queue.size()<=0)
+        {
+            cout<<"queue is empty, wait()..."<<endl;
+            _deqCv.wait(lock, [this]{return _queue.size()>0;});
+            cout << "I am back when queue size is " <<_queue.size() <<endl;
+        }
+        MsgType& msg = _queue.front();
+        _queue.pop();
+        _enqCv.notify_one();
+        return msg;
+    }
+    int Size()
+    {
+        unique_lock<mutex> lock(_mutex);
+        return _queue.size();
+    }
+};
+
+struct CustomerTask
+{
+    string task;
+    float money;
+    CustomerTask(){}
+    CustomerTask(const CustomerTask& cp):task(cp.task),money(cp.money){}
+    void ExecuteTask()
+    {
+        if(money>0)
+            cout<<"Task "<<task<<" is executed at $"<<money<<endl;
+        else
+            cout<<"Bank closed because the price is $"<<money<<endl;
+    }
+};
+
+//typedef MsgQueue<CustomerTask> TaskQueueType;
+typedef MsgQueue<CustomerTask> TaskQueueType;
+typedef MutexSafe<TaskQueueType> TaskQueueSafe;
+class ThreadPool
+{
+private:
+    int _limit;
+    vector<thread*> _workerThreads;
+    TaskQueueType& _taskQueue;
+    bool _threadPoolStop=false;
+public:
+  
+    void ExecuteTask()
+    {
+        while(1)
+        {
+            CustomerTask task=_taskQueue.Dequeue();
+            task.ExecuteTask();
+            if(task.money<0)
+            {
+                _threadPoolStop=true;
+                _taskQueue.Enqueue(task);//tell other threads to stop
+            }
+            if(_threadPoolStop)
+            {
+                cout<< "thread finshed!"<<endl;
+                return;
+            }
+            //the sleep function simulates that the task takes a while to finish 
+            std::this_thread::sleep_for(std::chrono::milliseconds(rand()%100));
+        }
+    }
+
+        ThreadPool(TaskQueueType& taskQueue,int limit=3):_limit(limit),_taskQueue(taskQueue){
+        for(int i=0;i<_limit;++i)
+        {  //生成 _limit个线程对象，并放到vetor里面
+            _workerThreads.push_back(new thread(&ThreadPool::ExecuteTask, this));
+        }
+    }
+    ~ThreadPool(){
+        for(auto threadObj: _workerThreads)
+        {
+            if(threadObj->joinable())
+                {
+                    threadObj->join();
+                    delete threadObj;
+                }
+        }
+    }
+};
+
+
+void TestLeaderFollower()
+{
+    TaskQueueType taskQueue(5);
+    
+    ThreadPool pool(taskQueue,4);
+    //the sleep function simulates the situation that all  
+    //worker threads are waiting for the empty message queue at the beginning
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    for(int i=0;i<10;i++)
+    {
+        CustomerTask task;
+        task.money= i+1;
+        if(task.money >5)
+            task.task = "deposit $";
+        else
+            task.task = "withdraw $";    
+        taskQueue.Enqueue(task);
+    }
+
+}
+
 
 int main(int argc, char *argv[]) {
    
@@ -120,7 +312,8 @@ int main(int argc, char *argv[]) {
     std::cout << cr.style_list[0].name <<std::endl;
     std::cout << cr.sessionID << "/0.jpg" <<std::endl;
 
-
+    
+    TestLeaderFollower();
 
     //tensorrt part
     YoloV8Config config;

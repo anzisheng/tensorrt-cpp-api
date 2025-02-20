d5b3ece1a2304e493b9821cfc6aebc928822d617
anzisheng <anzisheng@163.com>
2025-02-20 10:13:42 +0800

    use jsoncpp can read the sessionID

--- src/server_ws.cpp
+++ src/server_ws.cpp
@@ -1,3 +1,15 @@
+#include "nlohmann/json.hpp"
+#include<string>
+#include<iostream>
+#include<fstream>
+#include <json/json.h>
+#include <iostream>
+#include <json/json.h>
+using namespace std;
+using namespace Json;
+using namespace std;
+using json =nlohmann::json;
+
 #include <websocketpp/config/asio_no_tls.hpp>
 
 #include <websocketpp/server.hpp>
@@ -9,78 +21,13 @@ typedef websocketpp::server<websocketpp::config::asio> server;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
-#include "nlohmann/json.hpp"
-#include<string>
-#include<iostream>
-#include<fstream>
-#include <json/json.h>
-#include <iostream>
-using namespace std;
-using namespace Json;
-using namespace std;
-using json =nlohmann::json;
 
 // pull out the type of messages sent by our config
 typedef server::message_ptr message_ptr;
-namespace ns
-{
-    class student
-    {
-    public:
-         string name;
-    int age;
-    string phone;
-};
-void from_json(const json& j,student&p)
-{
-    j.at("name").get_to(p.name);
-    j.at("age").get_to(p.age);
-    j.at("phone").get_to(p.phone);
-}
-
-void to_json(json& j,const student& s)
-{
-    j = json{{"name",s.name},{"age",s.age},{"phone",s.phone}};
-}
-
-class class_room
-{
-public:
-string teacher_name;
-std::vector<student> student_list;
-
-};
-
-void from_json(const json& j, class_room& p)
-{
-
-j.at("teacher_name").get_to(p.teacher_name);
-
-for(auto &student_t:j["student_list"])
-{
-student s;
-from_json(student_t,s);
-p.student_list.push_back(s);
-
-}
-}
-void to_json(json&j, const class_room& s)
-{
-j = json{ {"teacher_name", s.teacher_name } };
-for(auto& student_t:s.student_list)
-{
-json j_student;
-to_json(j_student,student_t);
-j["student_list"].push_back(j_student);
-}
-}
 
 
-}
-
-// namespace 
-namespace ns
-{
+//namespace ns
+//{
     class style
     {
     public:
@@ -110,7 +57,7 @@ namespace ns
 
     void from_json(const json& j, order& p)
     {
-        std::cout << "ns from_json to object" <<std::endl;
+        std::cout << "from_json to object" <<std::endl;
         std::cout <<  j <<std::endl;
         std::cout <<  j.at("sessionID") <<std::endl;
         std::cout <<  "sessionID BEgin"  <<std::endl;
@@ -145,8 +92,7 @@ namespace ns
         }
     }
 
-}
-
+//}
 
 
 // Define a callback to handle incoming messages
@@ -154,29 +100,41 @@ void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
     std::cout << "on_message called with hdl: " << hdl.lock().get()
               << " and message: " << msg->get_payload()
               << std::endl;
-    std::cout << msg->get_payload().data() << std::endl;
-    std::cout << "The shit message from clientddddd:"<< std::endl;
-    std::cout << "The message from client from apifox:"<< std::endl;
+    std::cout << "The message from client:"<< std::endl;
+    std::cout << msg->get_payload()<<std::endl;
+    std::cout << "The message change to json:"<< std::endl;
+    std::cout << msg->get_payload().data()<<std::endl;
+    nlohmann::json commands = msg->get_payload().data();
+    std::cout << "to raw string:"<<commands << std::endl;
+    std::string jsonString = commands;
+    // 创建一个Json::CharReaderBuilder
+    Json::CharReaderBuilder builder;
 
-    //nlohmann::json commands;
-    //std::cout << commands << std::endl;
-    std::cout << "hello..."<< std::endl;
-    //Json::Value root = msg->get_payload().data();
-    //std::cout << root << std::endl;
-    std::cout << msg->get_payload().data() << std::endl;
-    //commands = nlohmann::json (msg->get_payload().data());//
-    //std::cout << commands << std::endl;
-    std::cout << "world..."<< std::endl;
-    ns::order cr;
-    std::cout << "cr..."<< std::endl;
-    //std::cout << commands << std::endl;
-    ns::from_json(msg->get_payload().data(), cr);
-    std::cout << "cr..."<< std::endl;
-    std::cout << "hello222..."<< std::endl;
-    std::cout << cr.style_list[0].name <<std::endl;
-    std::cout << cr.sessionID << "/0.jpg" <<std::endl;
+    // 创建一个Json::Value对象
+    Json::Value root;
+ 
+    // 创建一个错误信息字符串
+    std::string errors;
+    
+    // 解析JSON字符串
+    std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+    bool parsingSuccessful = reader->parse(jsonString.c_str(), jsonString.c_str() + jsonString.size(), &root, &errors);
+    if (!parsingSuccessful) {
+            // 打印错误信息并退出
+            std::cout << "Error parsing JSON: " << errors << std::endl;
+            //return 1;
+        }
+    
+    // 提取并打印数据
+    std::cout << "Name: " << root["sessionID"].asString() << std::endl;
 
-    std::cout <<"hello I am a server"<<std::endl;
+    //json commands = json(msg->get_payload().data())["sessionID"];
+    //std::cout << "the sessioId is :"<<commands.at("sessionID")<<std::endl;
+
+    //order cr;
+    //from_json(commands, cr);
+
+    std::cout <<"hello I am server"<<std::endl;
 
     // check for a special command to instruct the server to stop listening so
     // it can be cleanly exited.
@@ -193,24 +151,77 @@ void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
     }
 }
 
-
 int main() {
+
+    // // 创建一个Json::Value对象
+    // Json::Value root;
+ 
+    // // 向对象中添加数据
+    // root["name"] = "John Doe";
+    // root["age"] = 30;
+    // root["isAlive"] = true;
+    // root["address"]["city"] = "New York";
+    // root["address"]["state"] = "NY";
+ 
+    // // 创建一个Json::StreamWriterBuilder
+    // Json::StreamWriterBuilder writer;
+ 
+    // // 将Json::Value对象转换为字符串
+    // std::string output = Json::writeString(writer, root);
+ 
+    // // 打印输出
+    // std::cout << output << std::endl;
+ 
+    
+    // JSON字符串
+    //std::string jsonString = R"({"name":"John Doe","age":30,"isAlive":true,"address":{"city":"New York","state":"NY"}})";
+    std::string jsonString = R"({	
+	"sessionID":"1111111111111111111112222222222222",
+	"styleName":[
+	{
+		"name" : "1.jpg"		
+	},
+	{
+		"name" : "2.jpg"		
+	},
+	{
+		"name" : "3.jpg"
+	}
+	]	
+})";
+ 
+    // 创建一个Json::CharReaderBuilder
+    Json::CharReaderBuilder builder;
+ 
+    // 创建一个Json::Value对象
+    Json::Value root;
+ 
+    // 创建一个错误信息字符串
+    std::string errors;
+ 
+    // 解析JSON字符串
+    std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+    bool parsingSuccessful = reader->parse(jsonString.c_str(), jsonString.c_str() + jsonString.size(), &root, &errors);
+ 
+    if (!parsingSuccessful) {
+        // 打印错误信息并退出
+        std::cout << "Error parsing JSON: " << errors << std::endl;
+        return 1;
+    }
+ 
+    // 提取并打印数据
+    std::cout << "Name: " << root["sessionID"].asString() << std::endl;
+    // std::cout << "Age: " << root["age"].asInt() << std::endl;
+    // std::cout << "Is Alive: " << (root["isAlive"].asBool() ? "Yes" : "No") << std::endl;
+    // std::cout << "City: " << root["address"]["city"].asString() << std::endl;
+    // std::cout << "State: " << root["address"]["state"].asString() << std::endl;
+
+
+
     // Create a server endpoint
     server echo_server;
     std::cout << "hello, server!"<< std::endl;
-    Json::Value root; // root
-	Json::FastWriter writer;
-
-	Json::Value Person; // 子Value
-	Person["name"] = "tony";
-	Person["age"] = 22;
-	// 注意append方法的使用场景，只适用在添加数组item
-	root.append(Person);
-
-	string jsonStr = writer.write(root); // json到字符串
-
-	cout << "demo_write_simple ==============\n";
-	cout << jsonStr << endl;
+    
 
     try {
         // Set logging settings
--- src/server_ws_.cpp
+++ src/server_ws_.cpp
@@ -20,26 +20,15 @@ using namespace Json;
 using namespace std;
 using json =nlohmann::json;
 
-//#include "stdafx.h"
-#include "nlohmann/json.hpp"
-#include<string>
-#include<iostream>
-#include<fstream>
-
-using namespace std;
-using json =nlohmann::json;
-
 // pull out the type of messages sent by our config
 typedef server::message_ptr message_ptr;
-
-
 namespace ns
 {
     class student
     {
     public:
-    string name;
-    string age;
+         string name;
+    int age;
     string phone;
 };
 void from_json(const json& j,student&p)
@@ -89,20 +78,74 @@ j["student_list"].push_back(j_student);
 
 }
 
-
-void read_json_file(string file_name, json& commands)
+// namespace 
+namespace ns
 {
-ifstream jsonFile(file_name);
-jsonFile >> commands;
+    class style
+    {
+    public:
+        string name;
+        //int age;
+        //string phone;
+    };
+    void from_json(const json& j,style &p)
+    {
+        j.at("name").get_to(p.name);
+        //j.at("age").get_to(p.age);
+        //j.at("phone").get_to(p.phone);
+    }
+
+    void to_json(json& j,const style& s)
+    {
+        j = json{{"name",s.name}};
+    }
+
+    class order //class_room
+    {
+    public:
+    string sessionID;//teacher_name;
+    //std::vector<style> student_list;
+    std::vector<style> style_list;
+    };
+
+    void from_json(const json& j, order& p)
+    {
+        std::cout << "ns from_json to object" <<std::endl;
+        std::cout <<  j <<std::endl;
+        std::cout <<  j.at("sessionID") <<std::endl;
+        std::cout <<  "sessionID BEgin"  <<std::endl;
+        //std::string nameee = j.at(0);
+        json k = j["sessionID"];
+        std::cout <<  "sessionID000 end"  <<std::endl;
+
+        std::cout <<  k <<std::endl;
+        std::cout <<  "sessionID end"  <<std::endl;
+        j.at("sessionID").get_to(p.sessionID);
+        std::cout << p.sessionID <<std::endl;
+
+        for(auto &style_t:j["StyleImageName"])
+        {
+            //student s;
+            std::cout << style_t << std::endl;
+            style s;
+            from_json(style_t,s);
+            //p.student_list.push_back(s);
+            p.style_list.push_back(s);
+        }
+    }
+    //void to_json(json&j, const class_room& s)
+    void to_json(json&j, const order& s)
+    {
+        j = json{ {"sessionID", s.sessionID } };
+        for(auto& style_t:s.style_list)
+        {
+        json j_style;
+        to_json(j_style,style_t);
+        j["style_list"].push_back(j_style);
+        }
+    }
 
 }
-
-void write_json_file(string file_name, json& commands)
-{
-std::ofstream file(file_name);
-file << commands;
-}
-
 
 
 
@@ -124,14 +167,14 @@ void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
     //commands = nlohmann::json (msg->get_payload().data());//
     //std::cout << commands << std::endl;
     std::cout << "world..."<< std::endl;
-    // ns::order cr;
-    // std::cout << "cr..."<< std::endl;
-    // //std::cout << commands << std::endl;
-    // ns::from_json(msg->get_payload().data(), cr);
-    // std::cout << "cr..."<< std::endl;
-    // std::cout << "hello222..."<< std::endl;
-    // std::cout << cr.style_list[0].name <<std::endl;
-    // std::cout << cr.sessionID << "/0.jpg" <<std::endl;
+    ns::order cr;
+    std::cout << "cr..."<< std::endl;
+    //std::cout << commands << std::endl;
+    ns::from_json(msg->get_payload().data(), cr);
+    std::cout << "cr..."<< std::endl;
+    std::cout << "hello222..."<< std::endl;
+    std::cout << cr.style_list[0].name <<std::endl;
+    std::cout << cr.sessionID << "/0.jpg" <<std::endl;
 
     std::cout <<"hello I am a server"<<std::endl;
 
@@ -151,11 +194,23 @@ void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
 }
 
 
-
 int main() {
     // Create a server endpoint
     server echo_server;
     std::cout << "hello, server!"<< std::endl;
+    Json::Value root; // root
+	Json::FastWriter writer;
+
+	Json::Value Person; // 子Value
+	Person["name"] = "tony";
+	Person["age"] = 22;
+	// 注意append方法的使用场景，只适用在添加数组item
+	root.append(Person);
+
+	string jsonStr = writer.write(root); // json到字符串
+
+	cout << "demo_write_simple ==============\n";
+	cout << jsonStr << endl;
 
     try {
         // Set logging settings
@@ -181,55 +236,4 @@ int main() {
     } catch (...) {
         std::cout << "other exception" << std::endl;
     }
-}
-
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-int main2()
-{
-
-//////////////////////////////////
-server echo_server;
-std::cout << "hello, server!"<< std::endl;
-try {
-        // Set logging settings
-        echo_server.set_access_channels(websocketpp::log::alevel::all);
-        echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
-
-        // Initialize Asio
-        echo_server.init_asio();
-
-        // Register our message handler
-        echo_server.set_message_handler(bind(&on_message,&echo_server,::_1,::_2));
-
-        // Listen on port 9002
-        echo_server.listen(9002);
-
-        // Start the server accept loop
-        echo_server.start_accept();
-
-        // Start the ASIO io_service run loop
-        echo_server.run();
-    } catch (websocketpp::exception const & e) {
-        std::cout << e.what() << std::endl;
-    } catch (...) {
-        std::cout << "other exception" << std::endl;
-    }
-//////////////////////////
-    nlohmann::json commands;
-    read_json_file("test.json", commands);
-    cout << commands<<endl;
-    cout << "00000000" <<endl;
-    ns::class_room cr;
-    cout << "1111112222" <<endl;
-    ns::from_json(commands, cr);
-    cout << "1111113333" <<endl;
-    std::cout << cr.student_list[0].age <<std::endl;
-    cr.student_list[0].age +=10;
-    cout << "111111" <<endl;
-    json j_2;
-    ns::to_json(j_2, cr);
-    cout << "22222" <<endl;
-    std::cout<<"j_2:"<< j_2 << std::endl;
-return 0;
 }
